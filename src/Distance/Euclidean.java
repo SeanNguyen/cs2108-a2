87266551a8b6040f6b2065f2ca866121ba2c84b9
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-10-08 18:10:21 +0800

    added Euclidean distance implementation

--- src/Distance/Euclidean.java
+++ src/Distance/Euclidean.java
@@ -6,9 +6,17 @@ package Distance;
 public class Euclidean {
 
     public static double getDistance(double[] query1, double[] query2){
-        /**
-         * Please implement the distance/ similarity function by yourselves.
-         */
-        return 0.0;
+    	if (query1.length != query2.length){
+            System.err.println("The dimension of the two vectors does not match!");
+
+            System.exit(1);
+        }
+    	
+    	double sum = 0.0;
+    	for (int i = 0; i < query1.length; i++) {
+			sum += Math.pow(query1[i] + query2[i], 2);
+		}
+    	
+    	return -Math.sqrt(sum);
     }
 }
